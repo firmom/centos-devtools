be6d5f5751a1cc55534663285f51bd796d32cf8c
Sebastian Pozoga <sebastian@pozoga.eu>
2017-08-11 02:02:10 +0200

    correct startup script

--- image/etc/profile.d/startup.sh
+++ image/etc/profile.d/startup.sh
@@ -3,7 +3,7 @@ if [ ! -f "$HOME/.ssh/id_rsa" ]; then
   ssh-keygen -f "$HOME/.ssh/id_rsa" -t rsa -N ''
 fi
 
-if [ ! -f "$HOME/Desktop/autorun.sh" ]; then
+if [ -f "$HOME/Desktop/autorun.sh" ]; then
   chmod +x "$HOME/Desktop/autorun.sh"
   sh "$HOME/Desktop/autorun.sh"
 fi
